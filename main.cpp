8e12a2e41d1bd63869ec21b7f820f7b7ad50391a
zied hajji <hajji_zied@hotmail.fr>
2021-02-05 14:17:25 -0500

    Ajouter les fichiers

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    double largeur ;
+    int hauteur;
+    double aire;
+    do{
+	std::cout<< "Entrer la largeur du triangle : ";
+    std::cin>>largeur;
+    }while(largeur<=0);
+    do{
+    std::cout<< "Entrer la hauteur du triangle : ";
+    std::cin>>hauteur;
+    }while(hauteur<=0);
+    std::cout<<"largeur="<<largeur<<std::endl;
+    std::cout<<"hauteur="<<hauteur<<std::endl;
+    
+    std::cout<<"Aire="<<(hauteur*largeur)/2<<std::endl;
+}
